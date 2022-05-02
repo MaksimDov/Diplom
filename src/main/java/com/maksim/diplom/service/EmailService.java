1818899bf87c0aee1fec82daefc2f17d201787d9
MaksimDov <maksimdovydenko@mail.ru>
2022-05-02 20:49:47 +0700

    Added email confirmation, expanded range of categories.

---
+++ src/main/java/com/maksim/diplom/service/EmailService.java
@@ -0,0 +1,21 @@
+package com.maksim.diplom.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    public JavaMailSender emailSender;
+
+    public void sendSimpleMessage(String subject, String text, String to){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+}
